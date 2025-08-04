3e16b20fab24f1cd783671c154b001f46c3825d8
manoj <66349570+jmanojhcl@users.noreply.github.com>
2025-08-04 16:42:43 +0530

    updated

---
+++ src/test/java/com/example/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testMainMethodRuns() {
+        // This just checks that the main method doesn't throw an exception
+        HelloWorld.main(new String[]{});
+    }
+}
